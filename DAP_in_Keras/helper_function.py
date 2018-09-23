7472fad36596939a12cdef3730b5de75d1515aef
aaronyun <aaronyun.gongzuo568@gmail.com>
2018-09-23 15:10:37 +0800

    图片获取

--- DAP_in_Keras/helper_function.py
+++ DAP_in_Keras/helper_function.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 
 import io
+import os
 
 from skimage import io as skio
 import numpy as np
@@ -10,8 +11,18 @@ from sklearn import svm
 # 根据给出的图片类别名称读取这个类别的图片
 def batch_img_reader(batch_name):
 
+    # 用列表保存这一批图片
+    batch_img_array = []
+
+    # path是当前类别的路径，后面还要加上图片名字才能读取
     path = 'F:\\datasets\\AWA\\JPEGImages\\' + str(batch_name)
-    
-    
+    # 图片名字的列表
+    img_list = os.listdir(path)
+
+    for img_name in img_list:
+        # 图片的路径
+        img_path = path + str(img_name)
+        img_array = skio.imread(img_path)
+        batch_img_array.append(img_array)
 
     return batch_img_array
