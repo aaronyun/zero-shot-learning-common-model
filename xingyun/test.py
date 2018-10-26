4d2b49e4006194e784ff247c524b799fe754ffc7
aaronyun <aaronyun.gongzuo568@gmail.com>
2018-10-26 16:33:09 +0800

    提交测试

--- xingyun/test.py
+++ xingyun/test.py
@@ -4,11 +4,7 @@ from preprocess.img_and_attr_reader import read_and_expand_split_attr
 dataset_path = r'/data0/xingyun/AWA'
 
 # 特征提取函数测试
-<<<<<<< HEAD
 # feature_extractor(dataset_path, 'valid')
-=======
-feature_extractor(r'/data0/xingyun/AWA', 'valid')
->>>>>>> 05c6961ef22f3e40913ca31d4d756a12608bbd38
 # features = feature_reader('valid')
 # print(str(features.shape))
 
